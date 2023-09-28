9708916478d3ad7505269a229298bb37f9b39db6
“MarcoCosA” <marco.a.costafire@gmail.com>
2023-09-28 13:28:22 -0700

    Added blocks and trying to move index.html to main

--- design-blocks/custom_blocks.js
+++ design-blocks/custom_blocks.js
@@ -27,6 +27,51 @@ Blockly.Blocks['check_water_temperature'] = {
         this.setColour(120);
     }
 };
+
+Blockly.Blocks['enable_relay'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Enable Relay");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(150);
+    }
+};
+
+Blockly.Blocks['disable_relay'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Disable Relay");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(180);
+    }
+};
+
+Blockly.Blocks['read_sensor'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Read Sensor");
+        this.setOutput(true, "Number");
+        this.setColour(210);
+    }
+};
+
+Blockly.Blocks['number'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldTextInput("0", this.validateNumber), "NUM");
+        this.setOutput(true, "Number");
+        this.setColour(240);
+    },
+    validateNumber: function(number) {
+        // Ensure that the input is a valid number.
+        // If not, return null to reject the change.
+        return (isNaN(number) ? null : String(number));
+    }
+};
+
+
 Blockly.JavaScript['check_ph'] = function(block) {
     return 'sendPostRequest("/api/checkPH");\n';
 };
@@ -37,4 +82,21 @@ Blockly.JavaScript['check_water_level'] = function(block) {
 
 Blockly.JavaScript['check_water_temperature'] = function(block) {
     return 'sendPostRequest("/api/checkWaterTemperature");\n';
+};
+
+Blockly.JavaScript['enable_relay'] = function(block) {
+    return 'sendPostRequest("/api/enableRelay");\n';
+};
+
+Blockly.JavaScript['disable_relay'] = function(block) {
+    return 'sendPostRequest("/api/disableRelay");\n';
+};
+
+Blockly.JavaScript['read_sensor'] = function(block) {
+    return 'sendPostRequest("/api/readSensor");\n';
+};
+
+Blockly.JavaScript['number'] = function(block) {
+    var number = block.getFieldValue('NUM');
+    return [number, Blockly.JavaScript.ORDER_ATOMIC];
 };
