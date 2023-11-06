3cffe8060e74501431326996fc6484c25b0d8c13
ThomasKim13 <98933250+ThomasKim13@users.noreply.github.com>
2023-11-05 16:13:15 -0800

    Update arduinoAPI.ino

--- src/arduinoAPI.ino
+++ src/arduinoAPI.ino
@@ -1,29 +1,29 @@
 #include <WiFiS3.h>
 #include <DHT.h>
-#include "ph_iso_grav.h"       
-#include "ph_grav.h"             
+#include "ph_iso_grav.h"
+#include "ph_grav.h"
 #include "rtd_grav.h"
 
 #define WATER_LEVEL_PIN A2
 #define DHT11PIN 4
 #define DHTTYPE DHT11
 
-const char* ssid = "SpectrumSetup-EF";
-const char* password = "lightsnake383";
+const char *ssid = "SpectrumSetup-EF";
+const char *password = "lightsnake383";
 
 WiFiServer server(80);
 DHT dht(DHT11PIN, DHTTYPE);
 
 #ifdef USE_PULSE_OUT
-  Gravity_pH_Isolated pH(A0);         
+Gravity_pH_Isolated pH(A0);
 #else
-  Gravity_pH pH(A0);   
+Gravity_pH pH(A0);
 #endif
 
 Gravity_RTD RTD(A1);
 
-char user_data[32];                     
-uint8_t user_bytes_received = 0;                
+char user_data[32];
+uint8_t user_bytes_received = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -36,49 +36,58 @@ void setup() {
 
 void connectToWiFi() {
   WiFi.begin(ssid, password);
+  int attempts = 0;
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
+    attempts++;
+    if (attempts > 10) {
+      Serial.println("Failed to connect to WiFi");
+      // Handle WiFi connection failure here, e.g., retry or take appropriate action
+      break;
+    }
   }
-  Serial.println("Connected to WiFi");
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Connected to WiFi");
+  }
 }
 
 void pHSetup() {
   Serial.println(F("Use commands \"CAL,7\", \"CAL,4\", and \"CAL,10\" to calibrate the pH circuit"));
-  Serial.println(F("Use \"CAL,CLEAR\" to clear the pH calibration"));
-  if(pH.begin()) Serial.println("Loaded EEPROM for pH");
+  Serial.println(F("Use \"CAL,CLEAR\" to clear the pH calibration");
+  if (pH.begin()) Serial.println("Loaded EEPROM for pH");
 }
 
 void rtdSetup() {
-  Serial.println(F("Use \"CAL,nnn.n\" to calibrate the RTD circuit. \"CAL,CLEAR\" clears the RTD calibration"));
-  if(RTD.begin()) Serial.println("Loaded EEPROM for RTD");
+  Serial.println(F("Use \"CAL,nnn.n\" to calibrate the RTD circuit. \"CAL,CLEAR\" clears the RTD calibration");
+  if (RTD.begin()) Serial.println("Loaded EEPROM for RTD");
 }
 
 void getPublicIP(WiFiClient &client) {
-  const char* server = "api.ipify.org";
+  const char *server = "api.ipify.org";
   WiFiClient http;
-  
+
   if (!http.connect(server, 80)) {
     sendResponse(client, 500, "Error connecting to server");
     return;
   }
-  
+
   http.println("GET / HTTP/1.1");
   http.println("Host: api.ipify.org");
   http.println("Connection: close");
   http.println();
 
   while (http.connected() && http.readStringUntil('\n') != "\r") {}
-  
+
   String publicIP = http.readStringUntil('\r');
-  publicIP.trim();  
+  publicIP.trim();
 
   Serial.println("Public IP: " + publicIP);
   sendResponse(client, 200, publicIP);
   http.stop();
 }
 
-void parse_cmd(char* string) {
+void parse_cmd(char *string) {
   String cmd = String(string);
   cmd.toUpperCase();
   handlePHCommands(cmd);
@@ -89,18 +98,18 @@ void handlePHCommands(String &cmd) {
   if (cmd.startsWith("CAL,7")) {
     pH.cal_mid();
     Serial.println("MID CALIBRATED");
-  }
-  else if (cmd.startsWith("CAL,4")) {
+  } else if (cmd.startsWith("CAL,4")) {
     pH.cal_low();
     Serial.println("LOW CALIBRATED");
-  }
-  else if (cmd.startsWith("CAL,10")) {
+  } else if (cmd.startsWith("CAL,10")) {
     pH.cal_high();
     Serial.println("HIGH CALIBRATED");
-  }
-  else if (cmd.startsWith("CAL,CLEAR")) {
+  } else if (cmd.startsWith("CAL,CLEAR")) {
     pH.cal_clear();
     Serial.println("CALIBRATION CLEARED");
+  } else {
+    // Handle unrecognized pH command
+    Serial.println("Invalid pH command: " + cmd);
   }
 }
 
@@ -112,10 +121,12 @@ void handleRTDCommands(String &cmd) {
       if (param.equals("CLEAR")) {
         RTD.cal_clear();
         Serial.println("CALIBRATION CLEARED");
-      }
-      else {
+      } else if (param.toFloat() > 0.0) {
         RTD.cal(param.toFloat());
         Serial.println("RTD CALIBRATED");
+      } else {
+        // Handle invalid RTD calibration command
+        Serial.println("Invalid RTD calibration command: " + cmd);
       }
     }
   }
@@ -140,8 +151,8 @@ void loop() {
     user_bytes_received = Serial.readBytesUntil('\n', user_data, sizeof(user_data));
     if (user_bytes_received) {
       parse_cmd(user_data);
-      user_bytes_received = 0;                                                        
-      memset(user_data, 0, sizeof(user_data));                                         
+      user_bytes_received = 0;
+      memset(user_data, 0, sizeof(user_data));
     }
   }
 }
@@ -150,38 +161,51 @@ void handleClientRequest(WiFiClient &client) {
   String request = client.readStringUntil('\r');
   client.flush();
 
+  if (request.length() == 0) {
+    // Handle empty or invalid request
+    sendResponse(client, 400, "Bad Request");
+    return;
+  }
+
+  if (request.indexOf("HTTP/1.1") == -1) {
+    // Handle unsupported HTTP version
+    sendResponse(client, 505, "HTTP Version Not Supported");
+    return;
+  }
+
   if (request.indexOf("/LED_ON") != -1) {
     digitalWrite(LED_BUILTIN, HIGH);
     sendResponse(client, 200, "LED is on");
-  }
-  else if (request.indexOf("/LED_OFF") != -1) {
+  } else if (request.indexOf("/LED_OFF") != -1) {
     digitalWrite(LED_BUILTIN, LOW);
     sendResponse(client, 200, "LED is off");
-  }
-  else if (request.indexOf("/read_ph") != -1) {
+  } else if (request.indexOf("/read_ph") != -1) {
     float ph_value = pH.read_ph();
     sendResponse(client, 200, String(ph_value, 2));
-  }
-  else if (request.indexOf("/read_temp") != -1) {
+  } else if (request.indexOf("/read_temp") != -1) {
     float temp_value = RTD.read_RTD_temp_C();
-    sendResponse(client, 200, String(temp_value, 2));
-  }
-  else if (request.indexOf("/get_public_IP") != -1) {
+    if (temp_value != -127.0) {
+      sendResponse(client, 200, String(temp_value, 2));
+    } else {
+      // Handle RTD sensor reading error
+      sendResponse(client, 500, "RTD Sensor Reading Error");
+    }
+  } else if (request.indexOf("/get_public_IP") != -1) {
     getPublicIP(client);
-  }
-  else if (request.indexOf("/read_DHT") != -1) {
+  } else if (request.indexOf("/read_DHT") != -1) {
     float humidity = dht.readHumidity();
     float temperature = dht.readTemperature();
 
     if (!isnan(humidity) && !isnan(temperature)) {
       sendResponse(client, 200, String(temperature, 2) + "," + String(humidity, 2));
+    } else {
+      // Handle DHT sensor reading error
+      sendResponse(client, 500, "DHT Sensor Reading Error");
     }
-  }
-  else if (request.indexOf("/get_water_level") != -1) {
+  } else if (request.indexOf("/get_water_level") != -1) {
     int water_level = analogRead(WATER_LEVEL_PIN);
     sendResponse(client, 200, String(water_level));
-  }
-  else {
+  } else {
     sendResponse(client, 404, "Invalid request");
   }
 }
