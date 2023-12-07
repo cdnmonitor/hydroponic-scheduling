6be6600887e9f1e50c008f909fb631725fcb7fcd
“MarcoCosA” <marco.a.costafire@gmail.com>
2023-12-06 17:13:39 -0800

    Added executeCode button

--- src/design-blocks/app.js
+++ src/design-blocks/app.js
@@ -2,68 +2,76 @@ var workspace = Blockly.inject('blocklyDiv', {
     toolbox: document.getElementById('toolbox')
 });
 
-function sendPostRequest(apiEndpoint) {
-    return fetch(apiEndpoint, {
-        method: 'POST'
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-        })
-        .catch(error => {
-            const outputDiv = document.getElementById("outputDiv");
-            outputDiv.innerHTML += `${error.message}<br>`;
-            throw error;
-        });
-}
-
-
-function printToOutput(value) {
-    var outputDiv = document.getElementById("outputDiv");
-    var currentContent = outputDiv.innerHTML;
-    outputDiv.innerHTML = currentContent + value + "<br/>";
-}
-
-document.getElementById('runCode').addEventListener('click', function () {
+document.getElementById('executeCode').addEventListener('click', function () {
     // Generate JavaScript code from Blockly workspace
-    var generatedCode = Blockly.JavaScript.workspaceToCode(workspace);
-    document.getElementById("outputDiv").innerHTML = "";
-    try {
-        // Wrap generated code inside an async IIFE
-        var asyncCode = `(async () => {${generatedCode}})();`;
+    var generatedCode = generateAndProcessCode();
 
-        // Evaluate (run) the generated code
-        eval(asyncCode);
-    } catch (error) {
-        console.error('There was an error in the generated code:', error);
-    }
+    // Format the code for the server
+    var pseudocodeLines = generatedCode.split('\n'); // Assuming each line of code is a new line in the generated code
+
+    // Prepare the data to send
+    var dataToSend = JSON.stringify({ pseudocode: pseudocodeLines });
+
+    var userEndpointInputTxt = document.getElementById("userEndpointInput").value
+    // URL of your server endpoint
+    var serverUrl = `http://${userEndpointInputTxt}/executePseudocode`;
+
+    // Send the pseudocode to the server
+    fetch(serverUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: dataToSend
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response from the server
+            console.log('Success:', data);
+            document.getElementById("newOutputDiv").innerHTML += "Pseudocode sent successfully.";
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            document.getElementById("newOutputDiv").innerHTML += "Error executing pseudocode: " + error;
+        });
+
+    document.getElementById("newOutputDiv").innerHTML = `<pre>Sending: ${dataToSend} to ${serverUrl}</pre>`;
 });
 
-document.getElementById('generateCode').addEventListener('click', function () {
-    // Generate JavaScript code from Blockly workspace
+// Function to generate and process the code
+function generateAndProcessCode() {
     var generatedCode = Blockly.JavaScript.workspaceToCode(workspace);
     generatedCode = generatedCode.replace(/;\s*$/gm, '');
+
     if (typeof generatedCode !== 'string') {
         generatedCode = String(generatedCode);
     }
-    
+
     // Capitalize 'if' and 'else'
     generatedCode = generatedCode.replace(/if/g, 'IF');
     generatedCode = generatedCode.replace(/else/g, 'ELSE');
-    
+
     // Remove parentheses
     generatedCode = generatedCode.replace(/\(/g, '');
     generatedCode = generatedCode.replace(/\)/g, '');
-    
+
     // Remove curly brackets
     generatedCode = generatedCode.replace(/\{/g, '');
     generatedCode = generatedCode.replace(/\}/g, '');
-    
-    // Display the generated code in the newOutputDiv
-    document.getElementById("newOutputDiv").innerHTML = `<pre>${generatedCode}</pre>`;
-    
+
+    generatedCode = addEndIfStatements(generatedCode);
+
+    generatedCode = removeEmptyLines(generatedCode);
+
+    return generatedCode;
+}
+
+// Event listener for the button click
+document.getElementById('generateCode').addEventListener('click', function () {
+    var processedCode = generateAndProcessCode();
+    document.getElementById("newOutputDiv").innerHTML = `<pre>${processedCode}</pre>`;
 });
+
 
 function asyncYielder(genFunc) {
     const gen = genFunc();
@@ -81,13 +89,13 @@ function asyncYielder(genFunc) {
 
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('saveCode').addEventListener('click', function () {
-        let content = document.getElementById('newOutputDiv').textContent;
+        let content = generateAndProcessCode();
         let encodedUri = encodeURIComponent(content);
         let dataUri = 'data:text/plain;charset=utf-8,' + encodedUri;
 
         let link = document.createElement('a');
         link.setAttribute('href', dataUri);
-        link.setAttribute('download', 'outputCode.js');
+        link.setAttribute('download', 'outputCode.txt');
         document.body.appendChild(link);  // Required for Firefox
 
         link.click();  // This will download the text as an outputCode.txt file
@@ -95,4 +103,45 @@ document.addEventListener("DOMContentLoaded", function () {
         document.body.removeChild(link);  // Clean up after download
     });
 });
+function addEndIfStatements(code) {
+    const lines = code.split('\n');
+    const endIfStack = []; // Stack to keep track of 'IF' line indentations
+    const result = [];
 
+    lines.forEach((line, index) => {
+        const currentIndent = countIndentation(line);
+        const trimmedLine = line.trim();
+
+        if (trimmedLine.startsWith('IF')) {
+            endIfStack.push(currentIndent); // Push current indentation level to stack
+            result.push(line);
+        } else {
+            while (endIfStack.length > 0 && currentIndent <= endIfStack[endIfStack.length - 1]) {
+                result.push(generateEndIfLine(endIfStack.pop())); // Add 'END_IF' for each level we're exiting
+            }
+            result.push(line);
+        }
+    });
+
+    // Close any remaining 'IF' blocks
+    while (endIfStack.length > 0) {
+        result.push(generateEndIfLine(endIfStack.pop()));
+    }
+
+    return result.join('\n');
+}
+
+function countIndentation(line) {
+    const match = line.match(/^(\s*)/); // Match leading whitespace
+    return match ? match[1].length : 0; // Return length of whitespace
+}
+
+function generateEndIfLine(indentLevel) {
+    return ' '.repeat(indentLevel) + 'END_IF'; // Generate 'END_IF' line with appropriate indentation
+}
+
+function removeEmptyLines(text) {
+    return text.split('\n')
+        .filter(line => line.trim() !== '') // Filter out lines that are empty or contain only whitespace
+        .join('\n');
+}
